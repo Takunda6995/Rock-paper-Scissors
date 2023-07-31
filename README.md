6643a87dc47cc51b9bce0239b0b46f440473fa9f
Takunda6995 <takundamutare97@gmail.com>
2023-07-31 11:27:01 +0200

    added changes to conditions

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Rock-paper-Scissors
 Odin projection foundtion course javascript
+the program is run in an external javascript file.
+my idea was to create a working algorithm that prints the result to the console
+
+start
+Create arry of 3 string values
+create variable x-increment wnen player round is finished , i- increment when user wins round, j-increment when computer wins round
+do {
+Prompt the user to enter  the his choice of 1-rock 2-paper 3-scissors
+Store the input in a variable
+Subtract the variable by 1 and store in the variable userinput
+Use the maths class to generate  a number between 0 and 2 inclusive 
+Make sure the number is and interget
+Store the number generated in another  variable in the variabl comp input
+if userinput is not equal to compinput then
+ if userinput is greater than compinput 
+ then increment i and print player wins round 
+ else if userinput = 0 and compinput = 2    
+ then increment j and print computer wins
+ else if compinput =0 and user input =2 
+ then increment i and prnt player wins
+ else computer wins and increment j
+ if i is 3 or j is 3 then exit the while loop
+    increment x
+else
+don not increment x
+)while(x less than 5)
+
+if i is greater than j 
+then print user wins 
+esle print computer wins
+
--- mechanics.js
+++ mechanics.js
@@ -1,9 +1,9 @@
 //create an arr which holds 3 elements rock paper and sissors
-let gameArry = ["Rock", "paper", "sissors"]
+let gameArry = ["Rock", "paper", "scissors"]
 let inputData = 0;  
 let i=0, j=0, x=0
 do {
-let userInput = prompt("choose : 1- rock  2- paper  3-sissors",inputData ,1)
+let userInput = prompt("choose : 1- rock  2- paper  3-scissors",inputData ,1)
 //console.log(gameArry[userInput-1])
 //using math.random to generate the random choice made by the computer
 function getRandomNumber(min , max){
@@ -11,22 +11,33 @@ function getRandomNumber(min , max){
     Math.floor(max)
    return Math.floor(Math.random()*(max-min+1)+min); 
 }
-console.log(gameArry[getRandomNumber(0,2)]);
+//storing the user input and random generated number between 0 and 2 (inclusive)
 let userChoice =userInput-1;
 let compChoice = getRandomNumber(0,2);
 if(userChoice!=compChoice){
-    if(userChoice >compChoice){
+    if(userChoice >compChoice){//if the player wins
         i++
-        console.log(`computer: ${gameArry[compChoice]} vs player: ${gameArry[userChoice]}`)
-        console.log(`player won ${i} rounds`)
+        console.log(`computer: ${gameArry[compChoice]} vs player: ${gameArry[userChoice]}`);
+        console.log(`player won ${i} rounds`);
     
-    }else{
-        j++
+    }else if( userChoice ===0 && compChoice===3){
+        i++
+        console.log(`computer: ${gameArry[compChoice]} vs player: ${gameArry[userChoice]}`);
+        console.log(`player won ${i} rounds`);
+
+    }else if(userChoice===3 && compChoice ===0){
+        j++;//if the computer wins
+        console.log(`computer: ${gameArry[compChoice]} vs player: ${gameArry[userChoice]}`);
+        console.log(`Computer won ${j} rounds`);
+
+        }
+    else{
+        j++//if the computer wins
         console.log(`computer: ${gameArry[compChoice]} vs player: ${gameArry[userChoice]}`)
         console.log(`Computer won ${j} rounds`)
 
     }
-    if(i===3||j===3)break;
+    if(i===3||j===3)break; //exit the loop when someone wins
     ++x;
 }
 } while(x<5)
